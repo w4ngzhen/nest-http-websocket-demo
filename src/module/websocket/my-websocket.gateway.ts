f6917e57534de24e43021330b291048c1bbdd97a
w4ngzhen <w4ngzhen@hotmail.com>
2022-11-24 14:18:07 +0800

    modify: 增减对事件”hello“的处理，并在控制台打印请求。

--- src/module/websocket/my-websocket.gateway.ts
+++ src/module/websocket/my-websocket.gateway.ts
@@ -1,8 +1,18 @@
-import {WebSocketGateway} from "@nestjs/websockets";
+import {MessageBody, SubscribeMessage, WebSocketGateway} from "@nestjs/websockets";
+import {BizException} from "../../common/biz-exception";
+import {ERR_REQ_FIELD_ERROR} from "../../common/return-code";
 
 @WebSocketGateway(4000, {
     transports: ['websocket']
 })
 export class MyWebSocketGateway {
+
+    @SubscribeMessage('hello')
+    hello(@MessageBody() reqData: { name: string }) {
+        if (!reqData || !reqData.name) {
+            throw BizException.create(ERR_REQ_FIELD_ERROR, 'data is empty');
+        }
+        console.log(JSON.stringify(reqData));
+    }
 
 }
